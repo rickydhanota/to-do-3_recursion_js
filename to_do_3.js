7692661174c0e7c9671b893ec6ebd604e4b18ace
Ricky <rickdhanota@gmail.com>
2020-08-17 22:10:51 -0700

    added several methods

--- to_do_3.js
+++ to_do_3.js
@@ -1,9 +1,71 @@
-//Given a sorted array and a value, recursively determine whether value is found within array. 
+// Recursive Binary Search
+// Given a sorted array and a value, recursively determine whether value is found within array. 
+// rBinarySearch([1,3,5,6],4) = false; 
+// rBinarySearch([4,5,6,8,12],5) = true.
 
-//rBinarySearch([1,3,5,6],4) = false; 
-
-//rBinarySearch([4,5,6,8,12],5) = true;
-
-function rBinarySearch(arr, value){
-    var search = 
+function rBinarySearch(arr, value) {
+    var mid = Math.floor(arr.length/2);
+    if(arr[mid] == value) {
+        return true;
+    }
+    else if(value < arr[mid] && arr.length > 1) {
+        return rBinarySearch(arr.slice(0, mid), value);
+    }
+    else if(value > arr[mid] && arr.length > 1) {
+        return rBinarySearch(arr.slice(mid, arr.length), value)
+    } else {
+        return false
+    }
 }
+
+console.log(rBinarySearch([1, 3, 5, 6], 4));
+console.log(rBinarySearch([4, 5, 6, 8, 12], 5));
+
+// Greatest Common Factor
+// Given two integers, create rGCF(num1,num2) to recursively determine Greatest Common Factor (the largest integer dividing evenly into both). Greek mathematician Euclid demonstrated these facts:
+// gcf(a,b) == a, if a == b;
+// gcf(a,b) == gcf(a-b,b), if a>b;
+// gcf(a,b) == gcf(a,b-a), if b>a.
+// Second: rework facts #2 and #3 to reduce stack consumption and expand rGCF’s reach. You should  be able to compute rGCF(123456,987654).
+
+function rGCF(param1, param2) {
+    if (param1 == param2) {
+        return param1;
+    }
+    if (param1 > param2) {
+        return rGCF(param1-param2, param2);
+    }
+    if (param1 < param2) {
+        return rGCF(param1, param2-param1);
+    }
+}
+
+console.log(rGCF(3, 7));   
+console.log(rGCF(18, 27)); 
+console.log(rGCF(49, 14)); 
+
+function GCF(param1, param2) {
+    if (param1 == param2) {
+        return param1;
+    }
+    if (param1 > param2) {
+        if ((param1/param2) % 1 === 0) {
+            return param2;
+        } else {
+            return GCF(param1-param2, param2);
+        }
+    }
+    if (param1 < param2) {
+        if ((param1/param2) % 1 === 0) {
+            return param2;
+        } else {
+            return GCF(param1, param2-param1);
+        }
+    }
+}
+console.log(GCF(123456, 987654));
+Files
+to_do_3.js	August 05, 2020	
+ No file chosen  or 
+Add Github Url
+Privacy Policy
