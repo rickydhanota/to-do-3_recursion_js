09578a652c2554c0423617a424632a5c5033ffc0
Ricky <rickdhanota@gmail.com>
2020-08-17 22:09:10 -0700

    recursion initial commit

---
+++ to_do_3.js
@@ -0,0 +1,9 @@
+//Given a sorted array and a value, recursively determine whether value is found within array. 
+
+//rBinarySearch([1,3,5,6],4) = false; 
+
+//rBinarySearch([4,5,6,8,12],5) = true;
+
+function rBinarySearch(arr, value){
+    var search = 
+}
